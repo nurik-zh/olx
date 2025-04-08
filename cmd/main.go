4927ba8b62741791c96c5d23d4c40ccac3a30332
Nursultan <nursultanzuginisov4@gmail.com>
2025-04-09 00:42:09 +0500

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"olxkz/config"
+	"olxkz/routes"
+)
+
+func main() {
+	r := gin.Default()
+
+	config.ConnectDatabase()
+	routes.RegisterProductRoutes(r)
+
+	r.Run(":8080")
+}
