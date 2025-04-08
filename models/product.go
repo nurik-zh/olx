2f0a9b65f081447a81bdcaec75b69a9a6d7609ed
Nursultan <nursultanzuginisov4@gmail.com>
2025-04-09 00:42:43 +0500

    Add files via upload

--- models/product.go
+++ models/product.go
@@ -1,1 +1,8 @@
+package models
 
+type Product struct {
+	ID          uint    `json:"id" gorm:"primaryKey"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
